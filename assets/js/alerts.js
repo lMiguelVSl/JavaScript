b9cbc025aedacd5412fd650fb1c230789404ef94
Miguel <sanchez.vc@hotmail.com>
2022-05-18 14:09:11 -0500

    first commit

---
+++ assets/js/alerts.js
@@ -0,0 +1,14 @@
+
+
+alert('Hola mundo');
+
+let nombre = prompt('how is your name?');
+console.log(nombre);
+console.log('**' + nombre + '**');
+
+const selection = confirm('Are you shure of that');
+console.log(selection);
+
+//this work into navegators because are part of the windows object but for example in Node dosen't work because node don't have the windows object
+
+ 
---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+
+
+/* alert('alert'); */
+
+let a=10;
+const c = 10;
+var miNombre = 'Miguel';
+//declaration "quick"
+ 
+let b=1,
+    d=2,
+    e=3,
+    f=b+d;
+
+//kind of console.
+/* console.log('b + d: ',f);
+console.error('b: ',b); //red
+console.warn('d: ',d); */ //yellow
+
+//obj in console
+/* console.log('');
+console.log({f});
+console.error({b}); //red
+console.warn({d}); //yellow */
+
+//console.table bring a array or an object 
+/* console.table({a,b,e,f}); */
+
+const saludo = c + d;
+
+
