83eb5de780b377a88da13c4863cbe5a3c665139b
Miguel <sanchez.vc@hotmail.com>
2022-06-18 12:06:42 -0500

    Event click pedir and player points change with the click

--- 02-Blackjack/assets/js/juego.js
+++ 02-Blackjack/assets/js/juego.js
@@ -8,6 +8,13 @@
 let deck = [];
 const tipos = ['C','D','H','S'];
 const especiales = ['A','J','Q','K'];
+let puntosJugador = 0,
+    puntosComputador = 0;
+
+//Referencias
+
+const btnPedir = document.querySelector('#btnPedir'); 
+const puntosJugadorIdx = document.querySelector('#puntosJugador');
 
 //crear barajas
 const crearDeck = () => {
@@ -32,15 +39,14 @@ const crearDeck = () => {
 
 crearDeck();
     
-//funcion give me a card
+//function give me a card
 const pedirCarta = () => {
     
     if (deck.length === 0) {
         throw 'No hay mas cartas';
     } 
-    const carta = deck.pop();
+    const carta = deck.pop(); 
     console.log(deck);
-    console.log(carta);
     return carta; //retorno la carta eliminada 
 }
 
@@ -49,11 +55,19 @@ const pedirCarta = () => {
 const valorCarta = (carta) => {
 
     const valor = carta.substring(0, carta.length-1); //solo leo los valores necesarios en este caso 
-    return (isNaN(valor))?
-              valor==='A'? 11:10 
-            : valor*1
+    return (isNaN(valor))? (valor==='A')? 11:10 : valor*1
 
 }
 
-const valor = valorCarta( pedirCarta() );
-console.log({valor});
+/* const valor = valorCarta( pedirCarta() );
+console.log({valor}); */
+
+//Events
+
+btnPedir.addEventListener('click', () => {
+
+    const carta = pedirCarta();
+    puntosJugador += valorCarta(carta);
+    puntosJugadorIdx.innerHTML = puntosJugador;
+
+});
